5aa4aaeb04543efcf106c742ee13cb2671f935a3
Saurav Yadav <yadavsaurav98512@gmail.com>
2025-05-30 15:08:26 +0000

    Hydration failed because the server rendered HTML didn't match the client. As a result this tree will be regenerated on the client. This can happen if a SSR-ed Client Component used:
    - A server/client branch `if (typeof window !== 'undefined')`.
    - Variable input such as `Date.now()` or `Math.random()` which changes each time it's called.
    - Date formatting in a user's locale which doesn't match the server.
    - External changing data without sending a snapshot of it along with the HTML.
    - Invalid HTML tag nesting.
    
    It can also happen if the client has a browser extension installed which messes with the HTML before React loaded.
    
    See more info here: https://nextjs.org/docs/messages/react-hydration-error
    
      ...
        <HotReload assetPrefix="" globalError={[...]}>
          <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
            <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
              <ReplaySsrOnlyErrors>
              <DevRootHTTPAccessFallbackBoundary>
                <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
                  <HTTPAccessFallbackErrorBoundary pathname="/" notFound={<NotAllowedRootHTTPFallbackError>} ...>
                    <RedirectBoundary>
                      <RedirectErrorBoundary router={{...}}>
                        <Head>
                        <link>
                        <script>
                        <script>
                        <script>
                        <RootLayout>
                          <html lang="en" suppressHydrationWarning={true}>
                            <body
                              className="inter_9e72d27f-module__JKMi0a__variable font-sans antialiased bg-background text-..."
    -                         inmaintabuse="1"
    -                         cz-shortcut-listen="true"
                            >
                        ...
            ...
    src/app/layout.tsx (26:7) @ RootLayout
    
      24 |   return (
      25 |     <html lang="en" suppressHydrationWarning>
    > 26 |       <body className={`${inter.variable} font-sans antialiased bg-background text-foreground`}>
         |       ^
      27 |         <AppProviders>
      28 |           {children}
      29 |           <Toaster />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} font-sans antialiased bg-background text-foreground`}>
+      <body className={`${inter.variable} font-sans antialiased bg-background text-foreground`} suppressHydrationWarning>
         <AppProviders>
           {children}
           <Toaster />
